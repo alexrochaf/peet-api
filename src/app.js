b22dcf973ef5eb3bcf734d550f378059e391a81e
alexrochaf <alexrocha.f@outlook.com>
2023-09-14 16:01:17 -0400

    🔨 fix run app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
-import db from "./db";
-import routes from "./routes";
+import db from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
+import manipulador404 from "./middlewares/manipulador404.js";
 
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
